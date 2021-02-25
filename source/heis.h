ab22591485e6bbbe03bd2598739785696348baa8
Ulrik Myklebust Ferstad <ulrikmferstad@hotmail.com>
2021-02-25 14:21:36 +0100

    commit12

--- source/heis.h
+++ source/heis.h
@@ -13,13 +13,22 @@
     int currentDestination;
     int bestillingsliste[10];
     
-    void enterWaitingMode()
-    void kjorTilNesteDestinasjon(int i); 
-    bool destinasjonNaadd(int i);
-    void settNyDestinasjon(int[] bestillingsliste );
-    bool tomBestillingsliste(bestillingsliste b);
-    void settRetning(int i, bool b);
-    
+    void enterWaitingMode(void);
+    void kjorTilNesteDestinasjon(int* currentDestination) ; 
+    bool destinasjonNaadd(int bestillingsListe[], int i);
+    void settNyDestinasjon(int bestillingsListe []  );
+    bool tomBestillingsliste(int b []);
+    void settRetning(int i, bool b, int currentFloor);
+    void undefinedManouver(enum State* state);
 
 
-/*Kjører til etasjen destinasjon*/
+            /* Code block that makes the elevator go up when it reach the botton
+            if(hardware_read_floor_sensor(0)){
+                hardware_command_movement(HARDWARE_MOVEMENT_UP);
+            }
+
+            /* Code block that makes the elevator go down when it reach the top floor
+            if(hardware_read_floor_sensor(HARDWARE_NUMBER_OF_FLOORS - 1)){
+                hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
+            }
+*/
---
+++ source/indicators.h
@@ -0,0 +1,27 @@
+
+ /*Lights are set and cleared like this: 
+            for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
+                //Internal orders 
+                if(hardware_read_order(f, HARDWARE_ORDER_INSIDE)){
+                    hardware_command_order_light(f, HARDWARE_ORDER_INSIDE, 1);
+                }
+
+                //Orders going up 
+                if(hardware_read_order(f, HARDWARE_ORDER_UP)){
+                    hardware_command_order_light(f, HARDWARE_ORDER_UP, 1);
+                }
+
+                //Orders going down 
+                if(hardware_read_order(f, HARDWARE_ORDER_DOWN)){
+                    hardware_command_order_light(f, HARDWARE_ORDER_DOWN, 1);
+                }
+            } 
+
+
+// All buttons must be polled, like this: 
+            for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
+                if(hardware_read_floor_sensor(f)){
+                    hardware_command_floor_indicator_on(f);
+                }
+            }
+*/
