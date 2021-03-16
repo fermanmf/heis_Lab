d5482e6ae9d09eb1af781b023de36f459220adfa
Ulrik Myklebust Ferstad <ulrikmferstad@hotmail.com>
2021-03-16 19:54:40 +0100

    cleaning up

--- source/main.c
+++ source/main.c
@@ -8,23 +8,6 @@
 #include "bestilling.h"
 #include "door.h"
 
-static void clear_all_order_lights(){
-    HardwareOrder order_types[3] = {
-        HARDWARE_ORDER_UP,
-        HARDWARE_ORDER_INSIDE,
-        HARDWARE_ORDER_DOWN
-    };
-
-    for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
-        for(int i = 0; i < 3; i++){
-            HardwareOrder type = order_types[i];
-            hardware_command_order_light(f, type, 0);
-        }
-    }
-}
-
-
- 
 int main(){
     h_initiateHardware();
     numOrders = 0;
