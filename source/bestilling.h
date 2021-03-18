d0f541d226af2347ec504a5f4d638ff3e4573300
odiePose <tobias.rotnes.aasen@gmail.com>
2021-03-18 17:00:14 +0100

    Merge branch 'main' of https://github.com/fermanmf/heis_Lab into main

--- source/bestilling.h
+++ source/bestilling.h
@@ -1,8 +1,20 @@
+/**
+ * @file 
+ * @brief This module contains functions that 
+ * handle orders and arranging them i the correct order.
+ *
+ */
 
 #pragma once
 
+/**
+ * @brief This @c enum type sets the different types of buttons.
+ * **/
 enum bestillingsType{opp, inn, ned};
 
+/**
+ * @brief This @c struct type sets the different types of buttons.
+ * **/
 struct bestilling
 {
     int etasje;                 // Etasje heisen skal til
@@ -10,40 +22,27 @@ struct bestilling
 
 };
 
+/**
+ * @brief This @c int  keeps track of the number of orders.
+ * **/
 int numOrders;
 
+/**
+ * @brief This function are constantly looking for orders and triggers when a button is pushed. 
+ * **/
 void o_lookForOrders();
 
-void o_orderProcessed();
+/**
+ * @brief This function returnes the next order from the array of awaiting orders.
+ * **/
+int o_returnNextOrder();
 
-void o_orderCheck();
-
-void o_arrangeOrder();
-
-void o_between();
-
-int o_findOrderToSameFloor();
-
-void o_putOrderFirst();
-
-void o_orderOverFirst();
-
-void o_orderBelowFirst();
-
-void o_checkPriority();
-
-void o_checkPriority2();
-
-void pushArrayBack();
-
-void pushArray();
-
-void orderSent();
-
-int o_returnNextOrder();      // Funksjon skal bare brukes hvis det finnes en bestilling
-
+/**
+ * @brief This function checks if the order is done and then pushed ahead the new in front of the array.
+ * **/
 void o_checkIfOrderDone();
 
-bool queueIsEmpty();
-
+/**
+ * @brief This function sets the number of order to 0, and also cleans the array and sets it to onlt zeros.
+ * **/
 void o_clearOrders();
